5292db3bc01b06f2b53b9d860d8af72a1daa8df7
Vivek Kumar <ervivekkumar677@gmail.com>
2024-05-18 21:34:04 +0530

    Error Handled

--- countries-search/src/components/countryCard/CountryCard.jsx
+++ countries-search/src/components/countryCard/CountryCard.jsx
@@ -1,66 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./countryCard.css";
-import axios from "axios";
 
 const CountryCard = () => {
-  // const [countries, setCountries] = useState([]);
-  // const [filteredCountries, setFilteredCountries] = useState([]);
-  // const [searchInput, setSearchInput] = useState("");
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch("https://restcountries.com/v3.1/all");
-  //       if (!response.ok) {
-  //         throw new Error("Something went wrong");
-  //       }
-  //       const data = await response.json();
-  //       setCountries(data);
-  //       console.log(data)
-  //       setFilteredCountries(data);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //       setError(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // const handleSearch = (e) => {
-  //   const searchValue = e.target.value;
-  //   setSearchInput(searchValue);
-  //   const filtered = countries.filter((country) => {
-  //     return country.name.common
-  //       .toLowerCase()
-  //       .includes(searchValue.toLowerCase());
-  //   });
-  //   setFilteredCountries(filtered);
-  // };
-
-  // return (
-  //   <div>
-  //     <input
-  //       className="search-input"
-  //       value={searchInput}
-  //       onChange={handleSearch}
-  //       type="text"
-  //       placeholder="Search for countries.."
-  //     />
-  //     {error ? (
-  //       <div>{error}</div>
-  //     ) : (
-  //       <div className="countryCard">
-  //       {filteredCountries.map((country) => (
-  //         <div key={country.name.cca3} className="country-card">
-  //           <img src={country.flags.png} alt={country.name.common} />
-  //           <p>{country.name.common}</p>
-  //         </div>
-  //       ))}
-  //     </div>
-  //     )}
-  //   </div>
-  // );
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [error, setError] = useState(null);
@@ -70,7 +11,7 @@ const CountryCard = () => {
       .then(response => response.json())
       .then(data => setCountries(data))
       .catch(error => {
-        console.error('Error fetching data:', error);
+        console.error('Error during fetching data:', error);
         setError('Failed to fetch data');
       });
   }, []);
